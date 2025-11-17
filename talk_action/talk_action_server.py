1ec62b5717f0aa68aaa2e2da8e1accf37db704f9
Pablo Schwarzenberg <pablo.schwarzenberg@unab.cl>
2025-11-17 19:29:49 -0300

    base

---
+++ talk_action/talk_action_server.py
@@ -0,0 +1,32 @@
+import rclpy
+from rclpy.action import ActionServer
+from rclpy.node import Node
+
+from interfaces.action import Talk
+
+class TalkActionServer(Node):
+
+    def __init__(self):
+        super().__init__('talk_action_server')
+        self._action_server = ActionServer(
+            self,
+            Talk,
+            'Talk',
+            self.execute_callback)
+
+    def execute_callback(self, goal_handle):
+        self.get_logger().info('Executing goal...')
+        result = Talk.Result()
+        return result
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    talk_action_server = TalkActionServer()
+
+    rclpy.spin(talk_action_server)
+
+
+if __name__ == '__main__':
+    main()
