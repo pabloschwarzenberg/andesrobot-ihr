af2478e53b37a41cfc7cb3eb3445e1aaaa024294
Pablo Schwarzenberg <psr@alcyone.local>
2025-11-25 12:11:44 -0300

    vision

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ sudo apt install ros-humble-behaviortree-cpp-v3
 sudo apt install ros-humble-behaviortree-cpp
 sudo apt install ros-humble-ament-cmake-python
 sudo apt install python3-ament-package
+pkg-config --modversion opencv4
