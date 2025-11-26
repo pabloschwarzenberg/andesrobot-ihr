aff79eebf4c062f62924bec76de2cf0c2e1ae332
pabloschwarzenberg <pablo.schwarzenberg@unab.cl>
2025-11-26 15:56:18 -0300

    ask

---
+++ receptionist/src/ask_action/ask_action/ask_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from ask_action.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ receptionist/src/ask_action/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'ask_action'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='milo',
+    maintainer_email='pablo.schwarzenberg@unab.cl',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'ask_node = ask_action.ask_node:main'
+        ],
+    },
+)
