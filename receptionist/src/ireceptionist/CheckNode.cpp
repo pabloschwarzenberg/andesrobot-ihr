9c94ffafe2458544c622812c7adccca9807ffbd1
Pablo Schwarzenberg <psr@alcyone.local>
2025-11-28 08:41:11 -0300

    nodes

--- receptionist/src/ireceptionist/CheckNode.cpp
+++ receptionist/src/ireceptionist/CheckNode.cpp
@@ -3,10 +3,9 @@
 NodeStatus CheckNode::tick()
 {
     auto node = rclcpp::Node::make_shared("node_checker");
-    std::string target_node_name = getInput<std::string>("name");
     std::vector<std::string> node_names = node->get_node_names();
 
-    auto it = std::find(node_names.begin(), node_names.end(), target_node_name);
+    auto it = std::find(node_names.begin(), node_names.end(), getInput<std::string>("name"));
     if (it != node_names.end())
     {
         return NodeStatus::SUCCESS;
