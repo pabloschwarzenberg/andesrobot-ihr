25547f5d4f53a98aa657e671bad40cc7420c4c84
Pablo Schwarzenberg <psr@alcyone.local>
2025-11-28 16:10:30 -0300

    dialogo

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ sudo apt install ros-humble-behaviortree-cpp-v3
 sudo apt install ros-humble-behaviortree-cpp
 sudo apt install ros-humble-ament-cmake-python
 sudo apt install python3-ament-package
+sudo pip3 install vosk
 pkg-config --modversion opencv4
